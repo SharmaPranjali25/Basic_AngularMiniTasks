7589c99a1bbf8ba19663ea2631117b8fa3a3c210
Pranjali Sharma <sharmapranjali115@gmail.com>
2026-01-31 22:27:11 +0530

    two way binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { Profile } from './profile/profile';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Profile],
+  imports: [Login, Profile, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,56 +13,72 @@ export class App {
   // title= "Pranjali Sharma AngularProject";
   // function_btn(){
   //   alert("button clicked!");
-  count:number=0;
-  handleincrement(){
-    this.count=this.count+1;
+  count: number = 0;
+  handleincrement() {
+    this.count = this.count + 1;
   }
-  handledecrement(){
-    this.count=this.count-1;
+  handledecrement() {
+    this.count = this.count - 1;
   }
-  handlereset(){
-    this.count=0;
+  handlereset() {
+    this.count = 0;
   }
-  name=""
-  displayName="";
-  getName(event:Event){
-    const val=(event.target as HTMLInputElement).value;
+  name = ""
+  displayName = "";
+  getName(event: Event) {
+    const val = (event.target as HTMLInputElement).value;
     //console.log(name);
-    this.name=val;
-   
-  }
-  showName(){
-    this.displayName=this.name;
+    this.name = val;
 
   }
-  setName(){
-    this.name="Pranjali";
+  showName() {
+    this.displayName = this.name;
+
+  }
+  setName() {
+    this.name = "Pranjali";
   }
 
 
   // if else statement | Control Flow
-  display=true;
-  toggleDiv=true;
+  display = true;
+  toggleDiv = true;
   //display=false;
-  hidebox(){
-    this.display=false;
+  hidebox() {
+    this.display = false;
   }
-  showbox(){
-    this.display=true;
+  showbox() {
+    this.display = true;
   }
-  toogleTwo(){
-    this.toggleDiv=!this.toggleDiv;
+  toogleTwo() {
+    this.toggleDiv = !this.toggleDiv;
   }
 
   // ELSE IF 
-  color=1;
-  handlecolor(val:number){
-    this.color=val;
+  color = 1;
+  handlecolor(val: number) {
+    this.color = val;
   }
-  handletext(event:Event){
-    this.color= parseInt((event.target as HTMLInputElement).value);
-    
-  }
+  handletext(event: Event) {
+    this.color = parseInt((event.target as HTMLInputElement).value);
 
   }
+
+  // TWO WAY BINDING 
+  // nametwo="Pranjs"
+  nametwo = "";
+
+  // TO DO LIST
+  task = "";
+  tasklist: { id: number, task: string }[] = [];
+  addTask() {
+    this.tasklist.push({ id: this.tasklist.length + 1, task: this.task });
+    this.task = '';
+    console.log(this.tasklist);
+
+  }
+  deleteTask(taskid: number) {
+    this.tasklist = this.tasklist.filter((item) => item.id != taskid);
+  }
+}
 
