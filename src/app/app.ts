c875c0383dc9df097092ab27af281b85e8c0433c
Pranjali Sharma <sharmapranjali115@gmail.com>
2026-01-31 09:04:45 +0530

    adding basics angular code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,68 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularMiniProjects');
-}
+  // title= "Pranjali Sharma AngularProject";
+  // function_btn(){
+  //   alert("button clicked!");
+  count:number=0;
+  handleincrement(){
+    this.count=this.count+1;
+  }
+  handledecrement(){
+    this.count=this.count-1;
+  }
+  handlereset(){
+    this.count=0;
+  }
+  name=""
+  displayName="";
+  getName(event:Event){
+    const val=(event.target as HTMLInputElement).value;
+    //console.log(name);
+    this.name=val;
+   
+  }
+  showName(){
+    this.displayName=this.name;
+
+  }
+  setName(){
+    this.name="Pranjali";
+  }
+
+
+  // if else statement | Control Flow
+  display=true;
+  toggleDiv=true;
+  //display=false;
+  hidebox(){
+    this.display=false;
+  }
+  showbox(){
+    this.display=true;
+  }
+  toogleTwo(){
+    this.toggleDiv=!this.toggleDiv;
+  }
+
+  // ELSE IF 
+  color=1;
+  handlecolor(val:number){
+    this.color=val;
+  }
+  handletext(event:Event){
+    this.color= parseInt((event.target as HTMLInputElement).value);
+    
+  }
+
+  }
+
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css',
+  //styles:['h1{background-color:black}']
+})
+export class Profile {
+
+
+}
